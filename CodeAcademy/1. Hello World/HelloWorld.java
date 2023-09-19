68566002221cf5767701eaeec3bd110d73617d10
Crisilda Jojo <crisildajo@gmail.com>
2023-09-19 11:23:12 +0100

    codeacademy

---
+++ CodeAcademy/1. Hello World/HelloWorld.java
@@ -0,0 +1,13 @@
+// File name and class name must match
+public class HelloWorld {
+// Think of classes as a single concept
+    public static void main(String[] args) {
+        // The main method lists our program tasks
+        // print statement
+        System.out.println("Hello World!");
+        System.out.println("Good morning!");
+        // using print instead of println prints everthing on the same line
+        System.out.print("Hello");
+        System.out.print("Good Morning");
+    }
+}
---
+++ CodeAcademy/1. Hello World/HelloYou.java
@@ -0,0 +1,8 @@
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("Hello Crisilda!");
+        // System is a built-in Java class that contains useful tools for our programs
+        // out is short for "output"
+        // println is short for "print line"
+    }
+}
