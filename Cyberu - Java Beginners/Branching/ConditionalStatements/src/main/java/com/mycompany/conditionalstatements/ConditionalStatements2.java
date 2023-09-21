87b5e27cc3ca9658fef94718c203c8f8b3bb4ffa
Crisilda Jojo <crisildajo@gmail.com>
2023-09-21 11:59:28 +0100

    if statement issues sorted

--- Cyberu - Java Beginners/Branching/ConditionalStatements/src/main/java/com/mycompany/conditionalstatements/ConditionalStatements2.java
+++ Cyberu - Java Beginners/Branching/ConditionalStatements/src/main/java/com/mycompany/conditionalstatements/ConditionalStatements2.java
@@ -3,16 +3,18 @@ package com.mycompany.conditionalstatements;
 import java.util.*;
 
 public class ConditionalStatements2 {
-    Scanner reader = new Scanner(System.in);
-    String input = reader.next();
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
 
-    if(input.equals("password"))
-    {
-        System.out.println("YES");
-    }
+        if(input.equals("password"))
+        {
+            System.out.println("YES");
+        }
 
-    else{
-        System.out.println("NO");
+        else{
+            System.out.println("NO");
+        }
     }
-}}
+}
 
